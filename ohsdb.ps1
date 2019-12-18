a61c84d5e53cd9ef9bb1f04584da58ec7d63d458
Tony West <47873286+Un4gi@users.noreply.github.com>
2019-12-17 23:59:55 -0800

    Add files via upload

---
+++ ohsdb.ps1
@@ -0,0 +1,9 @@
+# This script will be called via the WMI events and is used to monitor the $WatchFolder for changes to the token.
+# A separate powershell script will be required for each folder you want to watch (or you can modify as needed to combine into one).
+$WatchFolder = "C:\OHSDB\OHSDB\bin\tokens" # Change this directory as necessary
+$Files = (Get-ChildItem $WatchFolder).FullName
+if ($Files) {
+    Remove-Item $Files
+    New-Item -Path "C:\OHSDB\OHSDB\bin\tokens" -Name "NetWars{R25TY5}" -ItemType "file" -Value "NetWars{R25TY5}" # Change the file name as necessary. Value should not matter for Netwars purposes.
+    # Need to add check to add token if folder is empty
+}
